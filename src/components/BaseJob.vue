bf9a4c6b484b5fc7075a0f3ba2308ff13dcc1b06
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2022-03-26 15:57:35 +0700

    add component job

---
+++ src/components/BaseJob.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- job card -->
+  <div class="card card-side bg-base-100 shadow-xl">
+    <router-link to="/detail">
+      <figure class="w-52 h-72">
+        <img
+          src="https://marquettechamber.com/wp-content/uploads/2020/07/services-In-the-city-1024x683.jpg"
+          alt="Movie"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">ชื่อบริษัท</h2>
+        <p>คำอธิบาย</p>
+        <p>เงินเดือน</p>
+        <div class="card-actions justify-end space-x-5">
+          <button class="btn btn-ghost btn-circle text-red-600">
+            <i class="material-icons"> favorite_border </i>
+          </button>
+          <button class="btn btn-primary">สมัครงาน</button>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/BaseJobView.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- search criteria -->
+  <div class="hero h-40 bg-gray-300">
+    <div class="form-control grid grid-cols-4 gap-4 w-11/12 font-sans-thai">
+      <div class="input-group w-full">
+        <input
+          type="text"
+          placeholder="ใส่ชื่อตำแหน่งงานหรือชื่อบริษัท"
+          class="input input-bordered w-full"
+        />
+      </div>
+      <div class="input-group w-full">
+        <select class="select select-bordered w-full">
+          <option disabled selected>จังหวัด</option>
+          <option>กรุงเทพ</option>
+          <option>กระบี่</option>
+        </select>
+      </div>
+      <div class="input-group w-full">
+        <select class="select select-bordered w-full">
+          <option disabled selected>รูปแบบงาน</option>
+          <option>รายเดือน</option>
+          <option>พาร์ทไทม์</option>
+        </select>
+      </div>
+      <div class="input-group w-full">
+        <select class="select select-bordered w-full">
+          <option disabled selected>เงินเดือน</option>
+          <option>มากไปน้อย</option>
+          <option>น้อยไปมาก</option>
+        </select>
+      </div>
+      <button class="btn">ค้นหางาน</button>
+      <button class="btn btn-ghost">รีเซ็ตเงื่อนไข</button>
+    </div>
+  </div>
+  <!-- job card -->
+  <base-job></base-job>
+</template>
+
+<script>
+import BaseJob from "./BaseJob.vue";
+export default {
+  components: { BaseJob },
+};
+</script>
+
+<style>
+</style>
