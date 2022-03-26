c9989786b5a1e6483576b15fd6dbf45155a3937c
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2022-03-26 15:56:57 +0700

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,87 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="navbar bg-blue-900 font-sans-thai">
+    <div class="navbar-start">
+      <div class="dropdown dropdown-hover">
+        <label tabindex="0" class="btn btn-ghost btn-circle m-1 text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="
+            font-medium
+            menu menu-compact
+            dropdown-content
+            p-2
+            shadow
+            bg-base-100
+            rounded-box
+            w-52
+          "
+        >
+          <router-link to="/">หางาน</router-link>
+          <li><a>เมนู 2</a></li>
+          <li><a>เมนู 3</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="navbar-center">
+      <router-link to="/"><img class="h-12" src="./assets/Firework_LOGO-02.png"></router-link>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-ghost btn-circle text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
+      <button class="btn btn-ghost btn-circle text-white">
+        <div class="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+          <span class="badge badge-xs badge-primary indicator-item"></span>
+        </div>
+      </button>
+      <button class="btn btn-ghost btn-circle text-white">
+        <i class="material-icons"> account_circle </i>
+      </button>
+    </div>
+  </div>
+  <router-view />
 </template>
 
 <style>
