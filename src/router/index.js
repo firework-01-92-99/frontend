fec8aee411eca223dd9f591cacce91f31d51985d
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2022-03-26 15:57:18 +0700

    add path

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import JobPage from '../views/JobPage.vue'
+import JobDetail from '../views/JobDetail.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'JobPage',
+    component: JobPage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/detail',
+    name: 'JobDetail',
+    component: JobDetail
+  },
 ]
 
 const router = createRouter({
