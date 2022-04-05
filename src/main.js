0acc48d1db2da41bd21e0558a4b5257f8a38edee
Supichaya <supichaya.jb@mail.kmutt.ac.th>
2022-04-05 19:52:11 +0700

    set axios in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './assets/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App).use(router).use(VueAxios, axios).use(store)
+app.mount('#app')
