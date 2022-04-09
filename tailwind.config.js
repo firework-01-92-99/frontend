75e10f5360d83d916e5545785c846e22625beebc
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2022-04-09 18:45:15 +0700

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
       fontFamily: {
         'sans-thai': ['"IBM Plex Sans Thai"', 'cursive'],
       },
+      colors: {
+        'dark-blue': '#21283A',
+        'orange-1': '#FF7B19',
+        'orange-2': '#E3701B',
+        'gray-1': '#DEE0E9',
+        'gray-2': '#F4F4F4'
+      }
     },
   },
   plugins: [require("daisyui")],
