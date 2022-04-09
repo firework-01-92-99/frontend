74b175abc02485cbbf929af64af1cfc0ff15c25c
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2022-04-09 18:50:22 +0700

    add job application page

---
+++ src/views/JobApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-gray-2 h-screen font-sans-thai">
+    <p class="text-2xl font-semibold p-6">ติดตามสถานะการสมัครงาน</p>
+    <div class="overflow-x-auto">
+      <table class="table w-3/4 mx-auto">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th></th>
+            <th class="text-base">บริษัท</th>
+            <th class="text-base">ตำแหน่ง</th>
+            <th class="text-base">ที่อยู่</th>
+            <th class="text-base">สถานะ</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- row 1 -->
+          <tr>
+            <th>1</th>
+            <td>ชื่อบริษัท</td>
+            <td>ชื่อตำแหน่ง</td>
+            <td>ที่อยู่</td>
+            <td>สถานะ</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
