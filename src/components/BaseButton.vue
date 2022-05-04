bd079701430adf5c12e8b0caa4e7681b231ff579
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2022-05-04 14:06:23 +0700

    add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn border-0 px-14 h-11" :class="[bgcolor, txtcolor]">
+    {{ txtbutt }}
+  </button>
+</template>
+
+<script>
+export default { props: ["bgcolor", "txtcolor", "txtbutt"] };
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import './assets/tailwind.css'
+import BaseButton from './components/BaseButton.vue'
 
 
 const app = createApp(App).use(router).use(VueAxios, axios).use(store)
+app.component('base-button', BaseButton)
 app.mount('#app')
